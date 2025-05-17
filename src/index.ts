e808859cb4a7ce92670cb5bceb53d8e805037375
Shubhamxshah <ritikgujarat@gmail.com>
2025-05-17 12:27:18 +0530

    added get route

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { cors } from "hono/cors";
 import { dbConnect } from "./config/db-config";
 import trackMailRoute from "./api/track-mail";
 import sendMailRoute from "./api/sendMail";
+import getMailStatusRoute from "./api/get-mail-status";
 
 const app = new Hono();
 app.use(cors())
@@ -12,6 +13,7 @@ dbConnect();
 // routes 
 app.route('/track', trackMailRoute);
 app.route('/api', sendMailRoute);
+app.route("/status", getMailStatusRoute)
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
